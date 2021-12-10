6c7f78c3b0bdf227f7b0cd561308e78bc5cbb48b
游欣 <987339699@qq.com>
2021-12-10 14:24:11 +0800

    '1'

---
+++ ExamAPP/app/src/main/java/com/BasicInfoActivity.java
@@ -0,0 +1,14 @@
+package com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BasicInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_info);
+    }
+}
---
+++ ExamAPP/app/src/main/java/com/examapp/MathQuestionActivity4.java
@@ -0,0 +1,38 @@
+package com.examapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MathQuestionActivity3 extends AppCompatActivity {
+    private ImageView next3;
+    private CheckBox option_1;
+    private CheckBox option_2;
+    private CheckBox option_3;
+    private CheckBox option_4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_math_question3);
+        next3=findViewById(R.id.next3);
+        option_1=findViewById(R.id.option1);
+        option_2=findViewById(R.id.option2);
+        option_3=findViewById(R.id.option3);
+        option_4=findViewById(R.id.option4);
+
+        next3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MathQuestionActivity3.this,EndPracticeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
